cdd51bbfc5421e3b66fa2bae1e5c460c9fe11d50
BYugandharOfficial <bandiyugandhar2@gmail.com>
2025-08-10 11:54:41 +0530

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Inventory_Mnagement_System.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Inventory_Mnagement_System.Startup))]
+namespace Inventory_Mnagement_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
